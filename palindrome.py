45dde996dfaa7616c0134538f2a7d93c6d5f0def
Coratolo <coratolo@pop-os.localdomain>
2023-04-11 10:55:01 -0300

    actualizacion

--- palindrome.py
+++ palindrome.py
@@ -1,18 +1,22 @@
 import unittest
 
 def palindrome(word):
-    inversa = " "
-    for i in range(len(word),1,-1):
+    inversa = ""
+
+    for i in range(len(word)-1,-1,-1):
         inversa += word[i] 
-        
-    if inversa == word:
+    
+    if (inversa == word):
         return True
     else:
         return False
 
 class TestPalindrome(unittest.TestCase):
-    def test_palindrome_simple(self):
+    def test_palindrome_simple1(self):
         result = palindrome('neuquen')
+        self.assertEqual(result, True)
+    def test_palindrome_simple2(self):
+        result = palindrome('hola como omoc aloh')
         self.assertEqual(result, True)
 
 if __name__ == '__main__':
